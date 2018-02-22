d6dae635ad37006d4747d9af3bcbb851f369c892
James Cook <jamescook.work@gmail.com>
2018-02-22 11:16:33 -0500

    Complete Class 9 prep videos

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CheeseMVC.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace CheeseMVC.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public CheeseType Type { get; set; }
+
         public int CheeseId { get; set; }
         private static int nextId = 1;
 
